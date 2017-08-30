f6ed2a6d0ba448b44e35f9a417662be60e5af71f
Ascold1 <ascold96@gmail.com>
2017-08-30 03:24:38 +0300

    final upd 11

--- app/js/common.js
+++ app/js/common.js
@@ -11,6 +11,15 @@ $(document).ready(function(){
        });
     //Accordeon
     $(function(){
+        var first = $(".question_item").eq(0);
+        first.addClass("active");
+        
+        var first_text = first.find(".question__text").text();
+        console.log(first_text);
+        var first_title = first.find(".question__title").text();
+        $(".question_right .question__text").text(first_text);
+        $(".question_right .question_title__text").text(first_title);
+        
         $(".question_item").on('click', function(e){
              if ($(window).width() >= '630'){
                  e.preventDefault();
@@ -41,6 +50,44 @@ $(document).ready(function(){
                 }
             }
     });
+    //Timer
+ $(function(){
+     
+     function getTimeRemaining(endtime){
+          var t = Date.parse(endtime) - Date.parse(new Date());
+       
+          var seconds = Math.floor( (t/1000) % 60 );
+          var minutes = Math.floor( (t/1000/60) % 60 );
+          var hours = Math.floor( (t/(1000*60*60)) % 24 );
+          var days = Math.floor( t/(1000*60*60*24) );
+          return {
+           'total': t,
+           'days': days,
+           'hours': hours,
+           'minutes': minutes,
+           'seconds': seconds
+          };
+    }
     
-
+     var days = $(".forms_bottom__circle.days");
+     var hours = $(".forms_bottom__circle.hours");
+     var minutes = $(".forms_bottom__circle.minutes");
+     var seconds = $(".forms_bottom__circle.seconds");
+     
+     function initializeClock(){
+           var timeinterval = setInterval(function(){
+           var endtime = $(".time").text();
+           var t = getTimeRemaining(endtime);
+           days.text(t.days);
+           hours.text(t.hours);
+           minutes.text(t.minutes);
+           seconds.text(t.seconds);
+           if(t.total<=0){
+            clearInterval(timeinterval);
+           }
+          },1000);
+        }
+     initializeClock();
+ });
+    
 });
